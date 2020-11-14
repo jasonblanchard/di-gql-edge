9ac92e4336c145a3a0def49b57b555f23bba8a33
Jason Blanchard <jas.blanchard@gmail.com>
2020-11-14 15:34:16 -0500

    fix syntax

--- src/utils/timestampUtils.ts
+++ src/utils/timestampUtils.ts
@@ -2,7 +2,7 @@ import proto from '../messages';
 
 export function protobufTimestampToDtoTimestamp(timestamp?: proto.google.protobuf.ITimestamp | null ) {
   if (!timestamp?.seconds) return undefined;
-  if (Number(timestamp.seconds) !> 0) return undefined;
+  if (Number(timestamp.seconds) > 0 == false) return undefined;
   const milliseconds = Number(timestamp.seconds) * 1000;
   return new Date(milliseconds).toISOString();
 }
